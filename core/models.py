5e00bb11e7574b3a99ec0af638b734f90aadd12a
nazaralievas <sadanaza4@gmail.com>
2024-08-16 18:13:47 +0600

    --

--- core/models.py
+++ core/models.py
@@ -5,6 +5,15 @@ class Movie(models.Model):
     genre = models.CharField(max_length=100)
     poster = models.CharField(max_length=255)
     description = models.TextField()
-    
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=200)
+    comment = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+
     def __str__(self):
         return self.name
