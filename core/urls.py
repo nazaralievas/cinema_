6446895ecb4344437c06bcfa0fcdb4624522495e
nazaralievas <sadanaza4@gmail.com>
2024-08-16 17:01:32 +0600

    --

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import homepage, rules
+from .views import homepage, rules, movie_detail
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('movie_detail/<int:id>', movie_detail, name='movie_detail'),
     path('rules', rules, name='rules')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Movie
 
 def homepage(request):
-    return render(request, 'core/homepage.html')
+    movies = Movie.objects.all()
+    return render(request, 'core/homepage.html', {'movies': movies})
+
+
+def movie_detail(request, id):
+    movie = Movie.objects.get(id=id)
+    return render(request, 'core/movie_detail.html', {'movie': movie})
+
 
 def rules(request):
     return render(request, 'core/rules.html')
